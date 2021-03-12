16b5ab455f046114f0d7f205fcf63e24c045f30b
kennarddh <kennarddh@gmail.com>
2021-03-12 16:23:56 +0700

    javascript & realtime

--- index.php
+++ index.php
@@ -4,27 +4,6 @@
 
     date_default_timezone_set('Asia/Jakarta');
 
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-    CURLOPT_URL => 'http://api.weatherstack.com/current?access_key=8d31a59241d10e47a054c39562bf0329&query=Zimbabwe',
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => 'GET',
-    CURLOPT_HTTPHEADER => array(
-        'Cookie: __cfduid=df3376d14ce595c39624634958ad203b21615087088'
-    ),
-    ));
-
-    $response = curl_exec($curl);
-
-    curl_close($curl);
-
-
 
 
     function get_time($add) {
@@ -37,18 +16,55 @@
         $time = mktime($h + $add,$i,$s,$m,$d,$y);
         return $time;
     }
-    $time = get_time(6);
-    $city = explode(",", json_decode($response, true)["request"]["query"])[0];
-    $sql = "INSERT INTO weather VALUES ('', '$city', '$response', '$time')";
-    $query = $conn->query($sql);
-    if ($query)
-    {
-    }
-    else
-    {
-        print_r([$city, $response, $time]);
-    }
+    // $time = get_time(6);
+    // $city = explode(",", json_decode($response, true)["request"]["query"])[0];
+    // $sql = "INSERT INTO weather VALUES ('', '$city', '$response', '$time')";
+    // $query = $conn->query($sql);
 
+    // if ($query)
+    // {
+
+    // }
+    // else
+    // {
+    //     print_r([$city, $response, $time]);
+    // }
+
+    if (isset($_GET["submit"]))
+    {
+        $search = $_GET["search"];
+        $apiKey = "8d31a59241d10e47a054c39562bf0329";
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'http://api.weatherstack.com/current?access_key=' . $apiKey . '&query=' . $search,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Cookie: __cfduid=df3376d14ce595c39624634958ad203b21615087088'
+        ),
+        ));
+
+        $response = json_decode(curl_exec($curl), true);
+        
+        curl_close($curl);
+
+        $city = $response["location"]["name"];
+        $location = $response["request"]["query"];
+        $image = $response["current"]["weather_icons"][0];
+        $imageAlt = $response["current"]["weather_descriptions"][0];
+        $humidity = $response["current"]["humidity"];
+        $temperature = $response["current"]["temperature"];
+        $observationTime = $response["current"]["observation_time"];
+        $localTime = $response["location"]["localtime"];
+        
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -59,6 +75,72 @@
     <link rel="stylesheet" href="asset/bootstrap-5/css/bootstrap.min.css">
     <link rel="stylesheet" href="asset/css/style.css">
     <script src="asset/bootstrap-5/js/bootstrap.min.js"></script>
+    <script>
+        var localTimeGet, localTime;
+        var t, d;
+
+        window.onload = (function(){start()});
+
+        function start()
+        {
+            localTimeGet = document.getElementById("localTime");
+            localTime = localTimeGet.innerHTML + ":00";
+
+            t = localTime.split(/[- :]/);
+            
+            t.shift();
+            t.shift();
+            t.shift();
+            t.shift();
+
+            d = new Date(t[0], t[1]-1, t[2], t[3], t[4], t[5]);
+            
+            changeLocalTime();
+        }
+
+        function changeLocalTime()
+        {
+            d.setSeconds(d.getSeconds() + 1);
+
+            localTimeGet.innerHTML = "Local Time : " + d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+
+            setTimeout(changeLocalTime, 1000);
+        }
+
+        function changeTemperatureFormat()
+        {
+            var switches = document.getElementById("TemperatureCheck");
+
+            var temperature = document.getElementById("temperature");
+            
+            var celcius, farentheith;
+
+            if (switches.checked == true)
+            {
+                farentheith = temperature.innerHTML;
+
+                farentheith = farentheith.slice(2, -1);
+                
+                celcius = (farentheith - 32) * 5/9;
+
+                celcius = celcius.toFixed(2);
+
+                temperature.innerHTML = "C " + celcius + "&deg;";
+            }
+            else
+            {
+                celcius = temperature.innerHTML;
+
+                celcius = celcius.slice(2, -1);
+
+                farentheith = (celcius * 9/5) + 32;
+
+                farentheith = farentheith.toFixed(2);
+
+                temperature.innerHTML = "F " + farentheith + "&deg;";
+            }
+        }
+    </script>
     <title>weather</title>
 </head>
 <body>
@@ -66,18 +148,21 @@
         <div class="container-fluid">
             <div class="nav-item" id="navbarSupportedContent">
                 <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-primary" type="submit">Search</button>
+                    <input class="form-control me-2" type="search" name="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-primary" name="submit" type="submit">Search</button>
                 </form>
             </div>
             <div class="nav item">
-                <p>20/20/2020</p>
+                <p id="localTime">Local Time : <?= $localTime; ?></p>
+            </div>
+            <div class="nav item">
+                <p>Observation Time : <?= $observationTime; ?></p>
             </div>
             <div class="nav-item">
                 <form action="" method="get">
                     <div class="form-check form-switch">
-                        <label for="flexSwitchCheckDefault">C</label>
-                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" style="width: 100px; height: 50px;" checked>
+                        <label for="TemperatureCheck">C</label>
+                        <input class="form-check-input" onclick="changeTemperatureFormat()" type="checkbox" id="TemperatureCheck" style="width: 100px; height: 50px;" checked>
                     </div>
                 </form>
             </div>
@@ -87,10 +172,11 @@
         <div class="row">
             <div class="Absolute-Center is-Responsive">
                 <div class="col-sm-12 col-md-10 col-md-offset-1 icon">
-                    <img class="weather-icon" src="https://assets.weatherstack.com/images/wsymbols01_png_64/wsymbol_0004_black_low_cloud.png" alt="">       
-                    30&deg;
+                    <p class="location">Location : <?= $location; ?></p>
+                    <img class="weather-icon" src="<?= $image; ?>" alt="<?= $imageAlt; ?>">       
+                    <div id="temperature">C <?= $temperature; ?>.00&deg;</div>
 
-                    <p class="humidity">humidty 10%</p>
+                    <p class="humidity">humidity <?= $humidity; ?>%</p>
                 </div>  
             </div>    
         </div>
